9aaacb73de35b2a1aabd5135365d36bf719f6158
= <=>
2021-06-11 02:25:52 +0400

    change stuff

--- Crypto-Price/GetData.py
+++ Crypto-Price/GetData.py
@@ -1,12 +1,12 @@
 import requests
-from colorama import init as color_init
+from colorama import init as color_init, Fore
 color_init()
 
 # First get the non cypto-currencies from the website
-govt_currencies = requests.get("https://api.coinbase.com/v2/currencies").json()
+fiat = requests.get("https://api.coinbase.com/v2/currencies").json()
 
 # Use list comprehension to generate a list of all the non-cryptos
-govt_currencies = [i["id"] for i in govt_currencies["data"]]
+fiat = [i["id"] for i in fiat["data"]]
 
 
 # Here we get the list of all Currencies
@@ -19,25 +19,32 @@ crypto_currencies = list(exchange_rates["data"]["rates"].keys())
 
 # Finally we compare the the two above lists to get the crypto currencies
 crypto_currencies = [
-    val for val in crypto_currencies if val not in govt_currencies]
+    val for val in crypto_currencies if val not in fiat]
 
 #r = requests.get("https://api.coinbase.com/v2/prices/ETH-USD/buy")
 
 
 def get_price(crypto, base):
-    ''' Get the current price of the [crypto-currencies] 
+    ''' Get the current price of the [crypto-currencies] \
         in terms of the physical currency specified '''
 
-    #if crypto in crypto_currencies and base in govt_currencies:
-    curr_conv_code = crypto + "-" + base
-    get_url = "https://api.coinbase.com/v2/prices/" + curr_conv_code + "/buy"
-    r = requests.get(get_url).json()
-    print("1 \x1b[1;31;40m{base}\x1b[0m costs \x1b[0;34;40m{amount}\x1b[0m \x1b[1;32;40m{currency}\x1b[0m"\
-        .format(base=r["data"]["base"], amount=r["data"]["amount"], currency=r["data"]["currency"]))
+    # if crypto in crypto_currencies and base in fiat:
+    if crypto in crypto_currencies and base in fiat:
+        curr_conv_code = crypto + "-" + base
     
+        get_url = "https://api.coinbase.com/v2/prices/" + curr_conv_code + "/buy"
+        r = requests.get(get_url).json()
+    
+        print("1 {red}{base}{rst} costs {blue}{amount}{rst} {green}{currency}{rst}"
+            .format(rst=Fore.RESET, red=Fore.LIGHTRED_EX, blue=Fore.LIGHTCYAN_EX, green=Fore.LIGHTGREEN_EX,
+                    base=r["data"]["base"], amount=r["data"]["amount"], currency=r["data"]["currency"]))
+    else:
+        #TODO: cli prompt to enter correct
+        print("Check inputs")
 
-'''crp = input("Enter crypto")
-bas = input("ENter bas")
 
-get_price(crp, bas)'''
-get_price("ETH","USD")
+crp = input("Enter crypto: ")
+fiat = input("ENter fiat currency: ")
+
+get_price(crp, fiat)
+#get_price("ETH", "USD")
