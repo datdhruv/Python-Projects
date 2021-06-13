ea089d41d1d5151a8ae3209f988ba68a78113f2b
Dhruv <Dhruv>
2021-06-14 03:37:54 +0400

    add setup.py

---
+++ Crypto-Price/setup.py
@@ -0,0 +1,40 @@
+
+import pathlib
+from setuptools import setup
+
+
+__version__ = '0.0.1'
+__author__ = 'Dhruv'
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+
+setup(
+    name = 'crypto',
+    version = __version__,
+    description = 'A Crpyto price checker',
+    long_description = README,
+    long_description_content_type = 'text/markdown',
+    #url = 'https://github.com/clieg/pwy',
+    author = __author__,
+    license = 'GPLv3',
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages = ['CryptoPrice'],
+    include_package_data = True,
+    install_requires = ['art==5.2',
+    'certifi==2021.5.30',
+    'chardet==4.0.0',
+    'colorama==0.4.4',
+    'idna==2.10',
+    'requests==2.25.1',
+    'urllib3==1.26.5'],
+    entry_points = {'console_scripts': ['crypto=CryptoPrice.__main__:main']},
+)
