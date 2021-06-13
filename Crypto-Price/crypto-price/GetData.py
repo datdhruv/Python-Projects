9130c7ace9c332e62f69338c96fe01583731b58e
Dhruv <Dhruv>
2021-06-14 03:14:02 +0400

    upload

--- Crypto-Price/crypto-price/GetData.py
+++ Crypto-Price/crypto-price/GetData.py
@@ -15,6 +15,7 @@ fiat = requests.get("https://api.coinbase.com/v2/currencies").json()
 
 # Use list comprehension to generate a list of all the non-cryptos
 fiat = [i["id"] for i in fiat["data"]]
+# fiat -> list
 
 
 # Here we get the list of all Currencies
@@ -24,10 +25,9 @@ exchange_rates = requests.get(
 # Then we need to make a list for the same
 crypto_currencies = list(exchange_rates["data"]["rates"].keys())
 
-
 # Finally we compare the the two above lists to get the crypto currencies
-crypto_currencies = [
-    val for val in crypto_currencies if val not in fiat]
+crypto_currencies = [val for val in crypto_currencies if val not in fiat]
+# crypt-=0_currencies -> list
 
 #r = requests.get("https://api.coinbase.com/v2/prices/ETH-USD/buy")
 
@@ -36,23 +36,16 @@ def get_price(crypto, base):
     ''' Get the current price of the [crypto-currencies] \
         in terms of the physical currency specified '''
 
-    # if crypto in crypto_currencies and base in fiat:
-    if crypto in crypto_currencies and base in fiat:
-        curr_conv_code = crypto + "-" + base
+    # some string handling required for the GET command at the API side
+    curr_conv_code = crypto + "-" + base
     
-        get_url = "https://api.coinbase.com/v2/prices/" + curr_conv_code + "/buy"
-        r = requests.get(get_url).json()
+    get_url = "https://api.coinbase.com/v2/prices/" + curr_conv_code + "/buy"
+    r = requests.get(get_url).json()
     
-        print("1 {red}{base}{rst} costs {blue}{amount}{rst} {green}{currency}{rst}"
-            .format(rst=Fore.RESET, red=Fore.LIGHTRED_EX, blue=Fore.LIGHTCYAN_EX, green=Fore.LIGHTGREEN_EX,
-                    base=r["data"]["base"], amount=r["data"]["amount"], currency=r["data"]["currency"]))
-    else:
-        #TODO: cli prompt to enter correct
-        print("Check inputs")
+    output_str = ("1 {red}{base}{rst} costs {blue}{amount}{rst} {green}{currency}{rst}"
+        .format(rst=Fore.RESET, red=Fore.LIGHTRED_EX, blue=Fore.LIGHTCYAN_EX, green=Fore.LIGHTGREEN_EX,
+                base=r["data"]["base"], amount=r["data"]["amount"], currency=r["data"]["currency"]))
+
+    return output_str
 
 
-#crp = input("Enter crypto: ")
-#fiat = input("ENter fiat currency: ")
-
-#get_price(crp, fiat)
-get_price("ETH", "USD")
---
+++ Crypto-Price/crypto-price/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+from . import GetData
+from art import tprint
+
+#GetData.get_price("ETH", "USD")
+parser = argparse.ArgumentParser(
+    prog="crypto-price",
+    description= "crypto-price - A simple crypto price tool."
+)
+parser.add_argument("Crypto", choices=GetData.crypto_currencies, metavar="Crypto", type=str.upper)
+parser.add_argument("Fiat", default="USD", choices=GetData.fiat, metavar="Fiat", type=str.upper)
+
+# will maybe work on later
+# parser.add_argument("-l", "--list", required=False, action="store_true")
+
+parsed_args = parser.parse_args()
+
+def main():
+    #GetData.get_price(parsed_args.Crpyto, parsed_args.Fiat_Currency)
+    tprint(parsed_args.Crypto, font="rand")
+    print(GetData.get_price(parsed_args.Crypto, parsed_args.Fiat))
+
+if __name__=="__main__":
+    main()
